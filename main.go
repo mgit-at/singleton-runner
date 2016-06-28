a4782dceaf92d0047d9b0dbe973912bc8f25a842
Christian Pointner <equinox@spreadspace.org>
2016-06-28 16:41:13 +0200

    added basic call structure

--- main.go
+++ main.go
@@ -20,7 +20,52 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path"
+	"path/filepath"
+	"syscall"
+	"time"
 )
+
+const (
+	LOCK_FILE_BASE = "/singleton.mgit.at/"
+)
+
+func runChild(cmd string, args []string, signals <-chan os.Signal) (err error) {
+	child := exec.Cmd{}
+	child.Path = cmd
+	child.Args = args
+	child.Stdin = os.Stdin
+	child.Stdout = os.Stdout
+	child.Stderr = os.Stderr
+
+	if err = child.Start(); err != nil {
+		return err
+	}
+
+	go func() {
+		for sig := range signals {
+			child.Process.Signal(sig)
+		}
+	}()
+	return child.Wait()
+}
+
+func acquireLock(lockfile string, ttl time.Duration) (err error) {
+	log.Printf("trying to acquire lock: %s (TTL: %v)", lockfile, ttl)
+	// TODO: try to create lock file, if it exists attach to it and wait for updated/deleted event
+	// when updated exit
+	// when deleted try again
+	return
+}
+
+func updateLock(lockfile string, ttl, timeout time.Duration) (err error) {
+	log.Printf("trying to update lock: %s (TTL: %v, Timeout: %v)", lockfile, ttl, timeout)
+	// Try to update the lock: return err if this fails
+	return
+}
 
 func main() {
 	var nameTemplate = flag.String("name-template", "", "template for the lockfile name (will get expanded using environment variables)")
@@ -32,20 +77,52 @@ func main() {
 	flag.Parse()
 
 	if *nameTemplate == "" {
-		log.Fatal("'-name-template' is empty")
+		log.Fatal("singleton-runner: '-name-template' is empty")
 	}
+	name := os.ExpandEnv(*nameTemplate)
+	lockfilePath := filepath.Join(LOCK_FILE_BASE, path.Clean("/"+name))
 
 	var cmd string
-	var args []string
-	if remaining := flag.Args(); len(remaining) > 0 {
-		cmd = remaining[0]
-		args = remaining[1:]
+	args := flag.Args()
+	if args := flag.Args(); len(args) > 0 {
+		cmd = args[0]
+	} else {
+		log.Fatal("singleton-runner: please specify a command to run")
 	}
 
-	log.Println("nameTemplate:", *nameTemplate)
-	log.Println("updateInterval:", *updateInterval)
-	log.Println("updateTimeout:", *updateTimeout)
-	log.Println("gracePeriod:", *gracePeriod)
-	log.Println("killDelay:", *killDelay)
-	log.Printf("command: %q %q", cmd, args)
+	ttl := time.Duration(*updateInterval+*updateTimeout+*gracePeriod+*killDelay) * time.Second
+
+	if err := acquireLock(lockfilePath, ttl); err != nil {
+		log.Fatal("singleton-runner: unable to acquier lock:", err)
+	}
+	exited := make(chan bool, 1)
+
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGHUP, syscall.SIGTERM)
+	go func() {
+		defer func() {
+			exited <- true
+		}()
+		err := runChild(cmd, args, signals)
+		if err != nil {
+			log.Printf("singleton-runner: child exited with: %v", err)
+		} else {
+			log.Printf("singleton-runner: child exited normally")
+		}
+	}()
+
+	t := time.NewTicker(time.Duration(*updateInterval) * time.Second)
+	for {
+		select {
+		case <-t.C:
+			if err := updateLock(lockfilePath, ttl, time.Duration(*updateTimeout)*time.Second); err != nil {
+				log.Println("singleton-runner: updateting lock failed:", err)
+				// send TERM signal to client
+				// if after gracePeriod child is still running send a KILL signal
+			}
+		case <-exited:
+			log.Println("singleton-runner: closing...")
+			return
+		}
+	}
 }
