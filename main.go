a24092d90c9c92be769e3de034d9501d8069829c
Christian Pointner <cpointner@mgit.at>
2016-06-29 12:18:19 +0200

    update, release lock works now

--- main.go
+++ main.go
@@ -57,11 +57,11 @@ func runChild(cmd string, args []string, signals <-chan os.Signal) (err error) {
 	return child.Wait()
 }
 
-func initETCdClient(updateTimeout time.Duration) (client.KeysAPI, error) {
+func initETCdClient(timeout time.Duration) (client.KeysAPI, error) {
 	cfg := client.Config{
 		Endpoints:               []string{"http://127.0.0.1:2379"}, // TODO: make this configurable
 		Transport:               client.DefaultTransport,
-		HeaderTimeoutPerRequest: updateTimeout,
+		HeaderTimeoutPerRequest: timeout,
 	}
 	c, err := client.New(cfg)
 	if err != nil {
@@ -78,11 +78,14 @@ func IsKeyExists(err error) bool {
 	return false
 }
 
-func acquireLock(kapi client.KeysAPI, lockfile string, ttl time.Duration) (err error) {
+func acquireLock(kapi client.KeysAPI, lockfile, instanceID string, ttl, timeout time.Duration) (err error) {
 	log.Printf("singleton-runner: trying to acquire lock: %s (TTL: %v)", lockfile, ttl)
 
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	opts := &client.SetOptions{PrevExist: client.PrevNoExist, TTL: ttl, Refresh: false}
-	if _, err = kapi.Set(context.Background(), lockfile, "kubernetes-pod-id", opts); err == nil { // TODO: use kubernetes pod name as value...
+	_, err = kapi.Set(ctx, lockfile, instanceID, opts)
+	cancel()
+	if err == nil {
 		return
 	}
 	if !IsKeyExists(err) {
@@ -96,20 +99,33 @@ func acquireLock(kapi client.KeysAPI, lockfile string, ttl time.Duration) (err e
 	return
 }
 
-func releaseLock(kapi client.KeysAPI, lockfile string) {
-	log.Printf("trying to release lock: %s", lockfile)
+func releaseLock(kapi client.KeysAPI, lockfile, instanceID string) {
+	log.Printf("singleton-runner: trying to release lock: %s", lockfile)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	opts := &client.DeleteOptions{PrevValue: instanceID}
+	_, err := kapi.Delete(ctx, lockfile, opts)
+	cancel()
+	if err != nil {
+		log.Printf("singleton-runner: releasing lock failed: %s", err)
+	}
+	return
 }
 
-func updateLock(kapi client.KeysAPI, lockfile string, ttl, timeout time.Duration) (err error) {
-	log.Printf("trying to update lock: %s (TTL: %v, Timeout: %v)", lockfile, ttl, timeout)
-	// Try to update the lock: return err if this fails
+func updateLock(kapi client.KeysAPI, lockfile, instanceID string, ttl, timeout time.Duration) (err error) {
+	log.Printf("singleton-runner: trying to update lock: %s (TTL: %v, Timeout: %v)", lockfile, ttl, timeout)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	opts := &client.SetOptions{PrevValue: instanceID, PrevExist: client.PrevExist, TTL: ttl, Refresh: false}
+	_, err = kapi.Set(ctx, lockfile, instanceID, opts)
+	cancel()
 	return
 }
 
 func main() {
 	var nameTemplate = flag.String("name-template", "", "template for the lockfile name (will get expanded using environment variables)")
+	var instTemplate = flag.String("instance-template", "", "template for the instance name (will get expanded using environment variables)")
 	var updateInterval = flag.Uint("update-interval", 30, "interval in seconds between lock file update requests")
-	var updateTimeout = flag.Uint("update-timeout", 5, "timeout in seconds to wait for response from etcd")
+	var requestTimeout = flag.Uint("request-timeout", 5, "timeout in seconds to wait for responses from etcd")
 	var gracePeriod = flag.Uint("grace-period", 30, "time in seconds to wait for a normal shutdown of the child")
 	var killDelay = flag.Uint("kill-delay", 5, "")
 
@@ -121,6 +137,11 @@ func main() {
 	name := os.ExpandEnv(*nameTemplate)
 	lockfilePath := filepath.Join(LOCK_FILE_BASE, path.Clean("/"+name))
 
+	if *instTemplate == "" {
+		log.Fatal("singleton-runner: '-instance-template' is empty")
+	}
+	instanceID := os.ExpandEnv(*instTemplate)
+
 	var cmd string
 	args := flag.Args()
 	if args := flag.Args(); len(args) > 0 {
@@ -129,15 +150,16 @@ func main() {
 		log.Fatal("singleton-runner: please specify a command to run")
 	}
 
-	ttl := time.Duration(*updateInterval+*updateTimeout+*gracePeriod+*killDelay) * time.Second
+	ttl := time.Duration(*updateInterval+*requestTimeout+*gracePeriod+*killDelay) * time.Second
+	etcdTimeout := time.Duration(*requestTimeout) * time.Second
 
-	kapi, err := initETCdClient(time.Duration(*updateTimeout) * time.Second)
+	kapi, err := initETCdClient(etcdTimeout)
 	if err != nil {
 		log.Fatal("error connecting to etcd:", err)
 	}
-	// defer releaseLock(kapi, lockfilePath) // should this be done in any case?
+	// defer releaseLock(kapi, lockfilePath, instanceID) // should this be done in any case?
 
-	if err := acquireLock(kapi, lockfilePath, ttl); err != nil {
+	if err := acquireLock(kapi, lockfilePath, instanceID, ttl, etcdTimeout); err != nil {
 		log.Fatal("singleton-runner: unable to acquire lock: ", err)
 	}
 
@@ -162,14 +184,14 @@ func main() {
 	for {
 		select {
 		case <-t.C:
-			if err := updateLock(kapi, lockfilePath, ttl, time.Duration(*updateTimeout)*time.Second); err != nil {
+			if err := updateLock(kapi, lockfilePath, instanceID, ttl, etcdTimeout); err != nil {
 				log.Println("singleton-runner: updateting lock failed:", err)
-				// send TERM signal to client
+				signals <- syscall.SIGTERM
 				// if after gracePeriod child is still running send a KILL signal
 			}
 		case <-exited:
 			log.Println("singleton-runner: closing...")
-			releaseLock(kapi, lockfilePath) // remove here if we do this using the defer above
+			releaseLock(kapi, lockfilePath, instanceID) // remove here if we do this using the defer above
 			return
 		}
 	}
